41bf3b2e21ab61606c29ad78d2f0a75726cf73ec
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-07-03 00:29:19 +0500

    some details changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/img/logo.svg";
 const Header = () => {
   return (
-    <header className="py-6 mb-12 border-b">
+    <header className="py-6 mb-12 border-b px-2">
       <div className="conatiner mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link to="/">
@@ -18,7 +18,7 @@ const Header = () => {
             Log in
           </Link>
           <Link
-            className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition sx:block hidden"
+            className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition "
             to=""
           >
             Sign Up
